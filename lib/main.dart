a115f96ba18fe6b466750627f225319727fb38c2
alamruslam4831 <alamruslam4831@gmail.com>
2024-06-19 04:09:18 +0000

    coba

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import "package:badges/badges.dart" as badges;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: badges.Badge(
+          badgeContent: Text(
+            '1',
+         style: GoogleFonts.montserrat(color: Colors.white),
+          ),
+          child: Icon(Icons.shopping_bag),
+        ),
+      ),
+    );
+  }
+}
